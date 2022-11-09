39e5d336f0f65bccd3f18f91d74a03cc5f8ee46a
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-11-09 23:46:34 +0300

    Add files via upload

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,19 @@
+﻿// ConsoleApplication9.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+// Чертоусова утренняя пробежка-2
+#include <iostream>
+using namespace std;
+int main()
+{  
+    int o, r, n, m;// орел герб колво монет монеты
+    cin >> n;
+    o = r = 0;  
+    for (int i = 0; i<n; i++) {
+        cin >> m;
+        if (m = 1) r++;
+        else n-=r;
+    }
+    cout << min(o,r);
+    
+}
+
